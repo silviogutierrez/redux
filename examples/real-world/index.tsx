6724647668bb01249e3ee19b84d616b50467b062
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2016-03-19 12:49:08 -0400

    Typings and typings and typings.

--- examples/real-world/index.tsx
+++ examples/real-world/index.tsx
@@ -1,5 +1,7 @@
+/// <reference path="./typings/browser.d.ts"/>
+
 import 'babel-polyfill'
-import React from 'react'
+import * as React from 'react'
 import { render } from 'react-dom'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
--- examples/real-world/webpack.config.js
+++ examples/real-world/webpack.config.js
@@ -5,12 +5,16 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './index'
+    './index.tsx'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
+  },
+  resolve: {
+      // Add `.ts` and `.tsx` as a resolvable extension.
+      extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
@@ -25,7 +29,7 @@ module.exports = {
         include: __dirname
       },
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' }
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' }
     ]
   }
 }
